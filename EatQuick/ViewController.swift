df3f0627dfa482daa1a87e12caf075f6864524d9
Kunal Pradhan <kpradhan@ucsd.edu>
2017-01-10 20:52:51 -0800

    initial commit

---
+++ EatQuick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EatQuick
+//
+//  Created by Kunal Pradhan on 1/10/17.
+//  Copyright © 2017 Kunal Pradhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
